f34db1a6368fb4b712ab7a79cddae7abcfc3dc9c
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2022-06-23 21:47:19 -0400

    Added perceive slidable widget

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.1.0] v0.1.0
+
+* Added Perceive Slidable Widget
+
 ## [0.0.2] v0.0.2
 
 * Updated license
--- lib/sliding_sheet.dart
+++ lib/sliding_sheet.dart
@@ -1,5 +1,6 @@
 library perceive_slidable;
 
+export 'src/perceive_slidable/perceive_slidable.dart';
 export 'src/sheet.dart';
 export 'src/sheet_listener_builder.dart';
 export 'src/specs.dart';
